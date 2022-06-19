08411bd2d8de3040e3b31ecb980df4829515245a
Siddharth <siddharth1stfeb@gmail.com>
2022-06-19 20:49:36 +0530

    added network files, api and dependency injection

---
+++ app/src/main/java/com/android/mynewsapp/data/model/NewsList.kt
@@ -0,0 +1,6 @@
+package com.android.mynewsapp.data.model
+
+import java.util.*
+
+data class NewsList (val articles:ArrayList<NewsItem>){
+}
---
+++ app/src/main/java/com/android/mynewsapp/other/Constant.kt
@@ -0,0 +1,5 @@
+package com.android.mynewsapp.other
+
+object Constant {
+    const val BASE_URL="https://saurav.tech/NewsAPI/"
+}
